b2c014a8654c27aff2f3f790c2e48308ffae20e4
Erica Wright and Sean Peterson <erica@>
2017-03-14 12:02:51 -0700

    update time display to reload and create alarm form

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -1,7 +1,25 @@
 var Clock = require('./../js/clock.js').clockModule;
 
+var displayTime = function(Time) {
+  $("#current-time").text(Time);
+  // setTimeout(function(){$('#time-we-want').val(Time)}, 20);
+
+  $('#time-we-want').val(Time);
+  // console.log(Time);
+};
+
 $(document).ready(function(){
-  var clock = new Clock;
-  var now = moment();
-  $("#current-time").text(clock.getLocalTime());
+  var clock = new Clock();
+  var currentTime = clock.getLocalTime(displayTime);
+  // var now = moment();
+  // var time = clock.getExactTime();
+  // console.log(time);
+  $('#alarm').submit(function(event){
+    event.preventDefault();
+    var alarmTime = $('#alarm-time').val();
+    var varr = '';
+    setTimeout(function(){
+    var varr = $('#time-we-want').val()}, 90);
+    // console.log(alarmTime);
+  });
 });
--- js/clock.js
+++ js/clock.js
@@ -3,14 +3,16 @@ var apiKey = require('./../.env').apiKey;
 function Clock() {
 }
 
-Clock.prototype.getLocalTime = function() {
-  // $.get('https://www.amdoren.com/api/timezone.php?api_key=Y5hxsN3nXGyQThdNn7zFXwxysE3UmL&loc=New+York')
-  $.get('http://api.timezonedb.com/v2/get-time-zone?key=' + apiKey + '&format=json&by=zone&zone=America/Los_Angeles')
-  .then(function(response) {
-    $("#current-time").text(response.formatted);
-  }).fail(function(error) {
-    $("#current-time").text(error.responseJSON.message);
-  });
+Clock.prototype.getLocalTime = function(displayTime) {
+  setInterval(function() {
+    $.get('http://api.timezonedb.com/v2/get-time-zone?key=' + apiKey + '&format=json&by=zone&zone=America/Los_Angeles')
+    .then(function(response) {
+      displayTime(response.formatted);
+    }).fail(function(error) {
+      $("#current-time").text("Error");
+    });
+
+  }, 1000);
 };
 
 exports.clockModule = Clock;
